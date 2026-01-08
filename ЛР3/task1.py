445acfd2bab0c3662c44ca12cb56821e861ee695
xdjet6 <urazbahtin.mm@edu.spbstu.ru>
2026-01-08 18:06:35 +0300

    ЛР3

---
+++ ЛР3/task1.py
@@ -0,0 +1,13 @@
+def index(items, finditems):
+    try:
+        return finditems.index(items)
+    except:
+        return None
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = index(find_item, items_list)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
