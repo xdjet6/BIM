246cabadb4239415c7c7152703928c0649001b16
xdjet6 <urazbahtin.mm@edu.spbstu.ru>
2026-01-20 16:55:37 +0300

    ЛР4

---
+++ ЛР4/task1.py
@@ -0,0 +1,15 @@
+import csv
+import json
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+def task() -> None:
+    with open(INPUT_FILENAME) as a:
+        lines = [line for line in csv.DictReader(a)]
+    with open(OUTPUT_FILENAME, 'w') as a:
+        json.dump(lines, a, indent=4)
+if __name__ == '__main__':
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
